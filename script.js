f1d628696b1191a9a755b2e567678fa414861530
Jorgesheahan <89272694+Jorgesheahan@users.noreply.github.com>
2023-06-15 17:15:18 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Get form values
+  var name = document.getElementById("name
