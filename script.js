8b8ac7e47b2d85eb5e41852bc7c0fff1515600f2
Jorgesheahan <89272694+Jorgesheahan@users.noreply.github.com>
2023-06-16 20:12:40 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-  event.preventDefault(); // Prevent form submission
-
-  // Get form values
-  var name = document.getElementById("name
